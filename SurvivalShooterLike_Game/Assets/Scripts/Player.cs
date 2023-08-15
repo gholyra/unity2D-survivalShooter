231e7bb4262af9c06c7a4e68df1518d090b98cd8
Tiago Marchetti Lyra <tiago.mlyra@gmail.com>
2023-08-15 20:54:53 -0300

    Aula 15/08
    Criação dos estados do controller da animação;
    Alteração no cenário;
    Criação de singletons e método da animação.

--- SurvivalShooterLike_Game/Assets/Scripts/Player.cs
+++ SurvivalShooterLike_Game/Assets/Scripts/Player.cs
@@ -1,3 +1,5 @@
+using JetBrains.Annotations;
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -8,11 +10,14 @@ public class Player : MonoBehaviour
     [SerializeField] private float velocity = 10;
     [SerializeField] private int lives = 3;
 
-    private Animator animator;
+    public Animator animator { get; private set; }
     public Transform playerTransform { get; private set; }
+    public bool isMoving { get; private set; }
+    public bool isHurt { get; private set; }
 
     private void Awake()
     {
+        #region Singleton
         if (instance == null)
         { 
             instance = this;
@@ -21,30 +26,69 @@ public class Player : MonoBehaviour
         { 
             Destroy(this.gameObject);
         }
+        #endregion
         animator = GetComponent<Animator>();
         playerTransform = GetComponent<Transform>();
     }
 
-    // Start is called before the first frame update
     private void Start()
     {
 
     }
 
-    // Update is called once per frame
     private void Update()
+    {
+        MoveHandler();
+        LifeHandler();
+        AnimationHandler();
+    }
+
+    #region Handlers
+    private void MoveHandler()
     {
         float moveX = Input.GetAxisRaw("Horizontal") * velocity * Time.deltaTime;
         float moveY = Input.GetAxisRaw("Vertical") * velocity * Time.deltaTime;
         playerTransform.Translate(moveX, moveY, 0f);
+        isMoving = moveX != 0 || moveY != 0;
+    }
+
+    private void LifeHandler()
+    {
         if (lives <= 0)
         {
             Destroy(this.gameObject);
         }
     }
 
+    private void AnimationHandler()
+    {
+        bool isMovingAnimation = animator.GetBool("isMoving");
+        bool isHurtAnimation = animator.GetBool("isHurt");
+
+        if (isMoving && !isMovingAnimation)
+        {
+            animator.SetBool("isMoving", true);
+        }
+        else if (!isMoving && isMovingAnimation)
+        {
+            animator.SetBool("isMoving", false);
+        }
+
+        if (isHurt && !isHurtAnimation)
+        {
+            animator.SetBool("isHurt", true);
+            isHurt = false;
+        }
+        else if (!isHurt && isHurtAnimation)
+        {
+            animator.SetBool("isHurt", true);
+        }
+    }
+    #endregion
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        isHurt = true;
         lives--;
     }
 
